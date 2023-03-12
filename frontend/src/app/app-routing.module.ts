a649a7a41ca1908dcd73bd22b12607fe444052d9
Onan Thairu <thairuonan@gmail.com>
2023-03-12 16:22:04 +0300

    add profile page.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   {path:'signup', loadComponent: () => import('./auth/signup/signup.component').then(c => c.SignupComponent)},
   {path:'login', loadComponent: () => import('./auth/login/login.component').then(c => c.LoginComponent)},
   {path: 'ask-question', loadComponent: () => import('./ask-question/ask-question.component').then(c => c.AskQuestionComponent)},
+  {path: 'profile', loadComponent: () => import('./profile/profile.component').then(c => c.ProfileComponent)},
   {path:'**', loadComponent: () => import('./page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent)}
 ];
 
