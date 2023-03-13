bf6d5066a7a981020d175dd26119ca500d0080af
Onan Thairu <thairuonan@gmail.com>
2023-03-13 05:33:59 +0300

    add admin page.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,8 +5,9 @@ const routes: Routes = [
   {path:'', loadComponent: () => import('./home/home.component').then(c => c.HomeComponent)},
   {path:'signup', loadComponent: () => import('./auth/signup/signup.component').then(c => c.SignupComponent)},
   {path:'login', loadComponent: () => import('./auth/login/login.component').then(c => c.LoginComponent)},
-  {path: 'ask-question', loadComponent: () => import('./ask-question/ask-question.component').then(c => c.AskQuestionComponent)},
-  {path: 'profile', loadComponent: () => import('./profile/profile.component').then(c => c.ProfileComponent)},
+  {path:'ask-question', loadComponent: () => import('./ask-question/ask-question.component').then(c => c.AskQuestionComponent)},
+  {path:'profile', loadComponent: () => import('./profile/profile.component').then(c => c.ProfileComponent)},
+  {path:'admin', loadComponent: () => import('./admin/admin.component').then(c => c.AdminComponent)},
   {path:'**', loadComponent: () => import('./page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent)}
 ];
 
