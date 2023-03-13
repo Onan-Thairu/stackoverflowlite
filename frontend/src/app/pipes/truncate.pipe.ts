8c02143fa6d9d85e8295a6f1eef8753dc5ce68f0
Onan Thairu <thairuonan@gmail.com>
2023-03-13 12:51:06 +0300

    add truncate custom pipe.

---
+++ frontend/src/app/pipes/truncate.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate',
+  standalone: true
+})
+export class TruncatePipe implements PipeTransform {
+
+  transform(value: string, limit: number): string {
+    if (value.length > limit) {
+      return value.substring(0, limit) + '...'
+    } else {
+      return value
+    }
+  }
+
+}
+
+// Usage
+// <p>{{ longText | truncate: 20 }}</p>
+
