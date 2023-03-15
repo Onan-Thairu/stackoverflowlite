387dc0e5d1796e3a7f951879e6f357df71e20aae
Onan Thairu <thairuonan@gmail.com>
2023-03-15 10:49:02 +0300

    update tables.

---
+++ backend/database/tables/Comments.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Comments (
+    id VARCHAR(255) PRIMARY KEY,
+    description TEXT NOT NULL,
+    answer_id VARCHAR(255),
+    user_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (answer_id) REFERENCES Answers(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE NO ACTION
+);
--- backend/database/tables/Questions.sql
+++ backend/database/tables/Questions.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Questions (
-    id INT IDENTITY(1,1) PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     tried TEXT NOT NULL,
-    user_id INT NOT NULL,
+    user_id VARCHAR(255) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
 );
