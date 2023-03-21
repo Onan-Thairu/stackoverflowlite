d00975833e8c0884c4c8e2a65876310c7c904f30
Onan Thairu <thairuonan@gmail.com>
2023-03-21 15:01:30 +0300

    protect comment routes

--- backend/src/routers/comment.router.ts
+++ backend/src/routers/comment.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { addComment, deleteComment, getAnswerComments, updateComment } from "../controllers/comment.controller";
+import { verifyToken } from "../middleware/verifyToken.middleware";
 
 export const commentRouter = Router()
 
-commentRouter.get("/:answer_id", getAnswerComments)
-commentRouter.post("/add-comment", addComment)
-commentRouter.put("/:comment_id", updateComment)
-commentRouter.delete("/:id", deleteComment)
+commentRouter.get("/:answer_id", verifyToken, getAnswerComments)
+commentRouter.post("/add-comment", verifyToken, addComment)
+commentRouter.put("/:comment_id", verifyToken, updateComment)
+commentRouter.delete("/:id", verifyToken, deleteComment)
