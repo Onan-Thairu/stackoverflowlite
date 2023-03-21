0b0491be7ffa780dddf7c7d420050a000cec2bfb
Onan Thairu <thairuonan@gmail.com>
2023-03-21 14:59:03 +0300

    add middleware and protect user routes.

--- backend/dist/routers/user.router.js
+++ backend/dist/routers/user.router.js
@@ -2,11 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const user_controller_1 = require("../controllers/user.controller");
+const verifyToken_middleware_1 = require("../middleware/verifyToken.middleware");
 const userRouter = (0, express_1.Router)();
-userRouter.get("", user_controller_1.getAllUsers);
-userRouter.get("/:id", user_controller_1.getUserById);
+userRouter.get("", verifyToken_middleware_1.verifyToken, user_controller_1.getAllUsers);
+userRouter.get("/:id", verifyToken_middleware_1.verifyToken, user_controller_1.getUserById);
 userRouter.post("/register", user_controller_1.registerUser);
 userRouter.post("/login", user_controller_1.loginUser);
-userRouter.put("/:id", user_controller_1.updateUser);
-userRouter.delete("/:id", user_controller_1.deleteUser);
+userRouter.put("/:id", verifyToken_middleware_1.verifyToken, user_controller_1.updateUser);
+userRouter.delete("/:id", verifyToken_middleware_1.verifyToken, user_controller_1.deleteUser);
 exports.default = userRouter;
--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { deleteUser, getAllUsers, getUserById, loginUser, registerUser, updateUser } from "../controllers/user.controller";
+import { verifyToken } from "../middleware/verifyToken.middleware";
 
 const userRouter = Router()
 
-userRouter.get("", getAllUsers)
-userRouter.get("/:id", getUserById)
+userRouter.get("", verifyToken, getAllUsers)
+userRouter.get("/:id", verifyToken, getUserById)
 userRouter.post("/register", registerUser)
 userRouter.post("/login", loginUser)
-userRouter.put("/:id", updateUser)
-userRouter.delete("/:id", deleteUser)
+userRouter.put("/:id", verifyToken, updateUser)
+userRouter.delete("/:id", verifyToken, deleteUser)
 
 export default userRouter
